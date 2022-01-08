4d3dbe9fa216570c8a04dd66cd78277d13d4f098
bielfont <94993537+bielfont@users.noreply.github.com>
2022-01-08 22:05:22 +0100

    Update bascula.spec.js

--- bascula.spec.js
+++ bascula.spec.js
@@ -1,13 +1,2 @@
 
-const assert = requiere('chai').assert;
-const assert = requiere('chai').expect;
-const assert = requiere('chai').should();
-
-const Bascula = require('bascula');
-
-
-Should:
-foo.should.be.a( 'string' );
-foo.should.equal( 'bar' );
-foo.should.have.length( 3 );
 
